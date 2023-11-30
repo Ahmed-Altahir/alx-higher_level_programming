32304e39a8eff4a96ce95091c7c0b4d868b22100
ahmod <ahmedaltahir77@gmail.com>
2023-11-30 16:25:25 +0300

    mosmas

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == '__main__':
     import sys
 
-    cunt = len(sys.argv) - 1
-    if cunt == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print('0 arguments.')
-    elif cunt == 1:
+    elif count == 1:
         print('1 argument:')
     else:
-        print('{} arguments:'.format(cunt))
-    for n in range(cunt):
+        print('{} arguments:'.format(count))
+    for n in range(count):
         print('{}: {}'.format(n + 1, sys.argv[n + 1]))
