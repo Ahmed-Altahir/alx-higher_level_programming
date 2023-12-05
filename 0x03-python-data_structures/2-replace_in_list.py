77f486fa592ca383f310b56e44ca338859a51f3f
AHMEDbash <ahmedaltahir77@gmail.com>
2023-12-05 13:13:55 +0200

    ohaa gzgza

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
     if idx >= 0 and idx < len(my_list):
-        my_list.insert(idx, element)
-    return my_list
+        return my_list[idx]
+
+my_list[idx] = element
+return my_list
