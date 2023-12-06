591bc30721ad8afc526410e1c98e539d80f786c7
AHMEDbash <ahmedaltahir77@gmail.com>
2023-12-06 21:49:04 +0200

    ohaa gzgza

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     tmp = []
-    for n in matrix:
-        tmp.append(list(map(lambda n: n**2, n)))
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
     return (tmp)
