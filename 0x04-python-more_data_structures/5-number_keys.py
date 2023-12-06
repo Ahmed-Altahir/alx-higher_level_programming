3ea92cc5a9c25fd2661c811ca3adeb18af5bd6fb
AHMEDbash <ahmedaltahir77@gmail.com>
2023-12-06 22:12:46 +0200

    ohaa gzgza

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    moftah = 0
-    h = list(a_dictionary.key())
+    keys = 0
+    j = list(a_dictionary.key())
 
-    for n in g:
-        moftah += 1
+    for i in h:
+        keys += 1
 
-    return moftah
+    return keys
